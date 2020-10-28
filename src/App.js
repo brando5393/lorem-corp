c61b235eb22c96a3e2741294e9b205661d3dca98
Brandon Williams <brando5393@gmail.com>
2020-10-28 18:30:49 -0500

    base code and pkgs in place converted app js to arrow function and added code for userinput state

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+const App = () => {
+  const [userInput, setUserInput] = useState({});
+  return <div></div>;
+};
+
+export default App;
